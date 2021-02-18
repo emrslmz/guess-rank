3668aab1f9c026be8a23c80c0180b9a65775e902
Emre Solmaz <emrslmz57@gmail.com>
2021-02-18 15:32:41 +0300

    adding footer

--- src/modules/home/assets/layouts/TheFooterHome.vue
+++ src/modules/home/assets/layouts/TheFooterHome.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    <p>
-      footer
-    </p>
+<footer>
+  <div class="d-flex justify-content-between align-items-center px-3 text-lightgray">
+      <div>
+       <h6> Guess Rank</h6>
+      </div>
+      <div class="d-flex flex-column">
+        Follow Us!
+       <div class="d-flex justify-content-between footer-social">
+         <a href="#"><i class="fab fa-twitter-square"></i></a>
+         <a href="#"><i class="fab fa-instagram"></i></a>
+         <a href="#"><i class="fab fa-facebook-f"></i></a>
+       </div>
+      </div>
   </div>
+</footer>
 </template>
+
+<style scoped>
+footer {
+  bottom: 0;
+  position: absolute;
+  min-height: 50px;
+  background-color: #191919;
+  width: 100%;
+}
+.footer-social i {
+  color: #d3d3d3;
+}
+</style>
--- src/modules/home/views/Home.vue
+++ src/modules/home/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="body-home">
     <the-header-home />
     <router-view />
     <the-footer-home />
@@ -18,3 +18,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.body-home {
+  height: 100vh;
+  /*background-color: #d3d3d3;*/
+  /* SUPERR */
+  /*background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;*/
+  /*background-blend-mode: multiply,multiply;*/
+  /* SUPERR */
+
+
+  background-image: linear-gradient(to top, #accbee 0%, #e7f0fd 100%);
+  background-blend-mode: multiply,multiply;
+
+  /*background-image: linear-gradient(to right, #868f96 0%, #596164 100%);*/
+}
+</style>
