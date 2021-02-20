c8f4ee2f0ec3a4fa3487ee12056ced921ba390f4
Emre Solmaz <emrslmz57@gmail.com>
2021-02-20 19:12:50 +0300

    adding playhome components

---
+++ src/modules/play/components/PlayHome.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <div class="container">
+    <div class="d-flex row justify-content-between">
+      <div class="col-12 col-xl-4">
+        <div class="card play-home-card-left"></div>
+      </div>
+
+      <div class="col-12 col-md-8">
+        <div class="card play-home-card-right"></div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+.card {
+  border-radius: 20px;
+  width: 100%;
+  min-height: 200px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+
+}
+
+/*@media screen and (max-width: 1600px) {*/
+/*  .play-home-card-left {*/
+
+/*  }*/
+/*  .play-home-card-right {*/
+/*  }*/
+/*}*/
+</style>
--- src/modules/play/views/Play.vue
+++ src/modules/play/views/Play.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container">
-    <div class="d-flex">
-      <p>fsdkjl</p>
-    </div>
+  <div>
+    <router-view />
+    <play-home />
   </div>
 </template>
+
+<script>
+import PlayHome from '@/modules/play/components/PlayHome.vue';
+
+export default {
+  name: 'Play',
+  components: {
+    PlayHome
+  }
+}
+</script>
