7bdd3800d850f6456040938575c31a404efff595
Emre Solmaz <emrslmz57@gmail.com>
2021-02-20 19:37:33 +0300

    minimal changes

--- src/modules/play/components/PlayHome.vue
+++ src/modules/play/components/PlayHome.vue
@@ -1,14 +1,27 @@
 <template>
 <div>
-  <div class="container">
+  <div class="px-3">
     <div style="padding-top: 100px"></div>
     <div class="d-flex row justify-content-between">
-      <div class="col-12 col-xl-4">
-        <div class="play-home-card"></div>
+      <div class="col-12 col-xl-6">
+        <div class="d-flex">
+          <div class="play-home-card mx-2"></div>
+          <div class="play-home-card mx-2"></div>
+        </div>
       </div>
 
-      <div class="col-12 col-md-8">
-        <div class="play-home-card"></div>
+      <div class="col-12 col-md-6">
+        <div class="play-home-card">
+          <div>
+            <h1>Play</h1>
+          </div>
+          <div class="d-flex justify-content-center align-items-center">
+            <div class="play-card"></div>
+            <div class="play-card"></div>
+            <div class="play-card"></div>
+            <div class="play-card"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +34,7 @@
   background-color: white;
   border-radius: 20px;
   width: 100%;
-  min-height: 200px;
+  min-height: 600px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
 }
 
@@ -32,4 +45,8 @@
 /*  .play-home-card-right {*/
 /*  }*/
 /*}*/
+
+.play-card {
+
+}
 </style>
