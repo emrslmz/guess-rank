4a35bfc643d5e60520c5b862443f02b9229bbf8e
Emre Solmaz <emrslmz57@gmail.com>
2021-03-23 16:57:33 +0300

    added admin login component

--- src/modules/admin/main/views/AdminHome.vue
+++ src/modules/admin/main/views/AdminHome.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    dsadsa
+  <div class="d-flex justify-content-center align-items-center pt-3">
+   <div class="text-center">
+     <h3 class="text-spacing5">Start</h3>
+     <small>You can switch quickly by clicking on the cards.</small>
+   </div>
   </div>
 </template>
 
@@ -9,3 +12,7 @@ export default {
   name: 'AdminHomePage',
 }
 </script>
+
+<style scoped>
+
+</style>
