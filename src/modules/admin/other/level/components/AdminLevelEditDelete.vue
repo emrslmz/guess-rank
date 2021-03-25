15822d2a79cec6e935e8728020e4546644456f46
Emre Solmaz <emrslmz57@gmail.com>
2021-03-25 23:51:10 +0300

    added adminleveledit component

--- src/modules/admin/other/level/components/AdminLevelEditDelete.vue
+++ src/modules/admin/other/level/components/AdminLevelEditDelete.vue
@@ -2,7 +2,7 @@
   <div class="d-flex justify-content-center align-items-center game-selected">
     <div class="container">
       <div class="text-center pt-5 mt-5">
-        <h4 class="text-spacing5">Game's Categories</h4>
+        <h4 class="text-spacing5">Level: {{ getLevelInfo.selectedLevelData.level_id }}</h4>
         <small>All Added Game Categories. Click """"" button for edit/delete.</small>
         {{ getLevelInfo.selectedLevelData}}
       </div>
@@ -11,7 +11,7 @@
           <div>
             <small>{{ getLevelInfo.selectedLevelData.level_random_key }}</small>
           </div>
-         <div class="d-flex justify-content-center align-items-center">
+         <div class="d-flex justify-content-center align-items-start">
 
            <!--LEFT-->
            <div class="d-flex flex-column">
@@ -39,7 +39,7 @@
                    <i class="fas fa-text-height"></i>
                  </div>
                  <div>
-                   <textarea cols="23" class="custom-textarea" v-model="getLevelInfo.selectedLevelData.level_description"  placeholder="Level Description"></textarea>
+                   <textarea cols="25" class="custom-textarea" v-model="getLevelInfo.selectedLevelData.level_description"  placeholder="Level Description"></textarea>
                  </div>
                </div>
              </div>
@@ -50,12 +50,12 @@
                <div class="d-flex justify-content-between align-items center pb-3 px-2">
                  <div class="d-flex flex-column justify-content-center center">
                    <div class="text-center">
-                     <small>Level Available</small>
+                     <small>Level Locked</small>
                    </div>
                    <div class="d-flex justify-content-center align-items-center">
-                     <span><i class="far fa-eye-slash"></i></span>
-                     <div><input class="dark-mode-button mt-2 mx-3" v-model="getLevelInfo.selectedLevelData.is_available" type="checkbox" /></div>
-                     <span><i class="far fa-eye"></i></span>
+                     <span><i class="fas fa-lock"></i></span>
+                     <div><input class="dark-mode-button mt-2 mx-3" v-model="getLevelInfo.selectedLevelData.is_locked" type="checkbox" /></div>
+                     <span><i class="fas fa-lock-open"></i></span>
                    </div>
                  </div>
                </div>
@@ -80,25 +80,89 @@
            <!--RIGHT-->
            <div class="d-flex flex-column">
 
+
              <div class="d-flex flex-column justify-content-center center pb-3 px-2">
-               <div class="text-center px-2">
-                 <small>Game Name</small>
+               <div class="text-left px-2">
+                 <small>Level Name</small>
+               </div>
+               <div class="d-flex justify-content-center align-items-center">
+                 <div class="custom-input-icon">
+                   <i class="fas fa-sort-numeric-up-alt"></i>
+                 </div>
+                 <div>
+                   <input class="custom-input" v-model="getLevelInfo.selectedLevelData.level_id"  placeholder="Level Number" type="number">
+                 </div>
+               </div>
+             </div>
+
+             <div class="d-flex flex-column justify-content-center center pb-3 px-2">
+               <div class="text-left px-2">
+                 <small>Select Game</small>
                </div>
                <div class="d-flex justify-content-center align-items-center ">
                  <div class="custom-input-icon">
                    <i class="fas fa-gamepad"></i>
                  </div>
                  <div>
-                   <input class="custom-input"   placeholder="Game names" type="text">
+                   <select class="custom-select" v-model="getLevelInfo.selectedLevelData.game_id">
+                     <option :value="game.game_id" v-for="(game, index) in getGameInfo.gameData" :key="index">{{ game.game_name }}</option>
+                   </select>
                  </div>
                </div>
+             </div>
+
+             <div class="create-update-date">
+               <h5><i class="far fa-clock"></i> {{ getLevelInfo.selectedLevelData.created_at }}</h5>
+               <h5 class="text-center" v-if="getLevelInfo.selectedLevelData.created_at === getLevelInfo.selectedLevelData.updated_at"><i class="far fa-calendar-times"></i> Change record not found!</h5>
+               <h5 v-else>{{ getLevelInfo.selectedLevelData.updated_at }}</h5>
              </div>
            </div>
            <!--/RIGHT-->
          </div>
 
+          <div class="d-flex">
+            <div class="d-flex justify-content-center">
+              <div class="green-success-button">
+                <button class="btn btn-light-success btn-xl w-100">Save</button>
+              </div>
+            </div>
+
+            <div class="d-flex justify-content-center px-2">
+              <div class="green-success-button">
+                <button class="btn bg-danger btn-xl w-100" data-toggle="modal" data-target="#deleteGame">Delete</button>
+              </div>
+            </div>
+          </div>
+
 
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="deleteGame"  aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title text-left" id="exampleModalLabel">Are you sure you want to delete the item named ?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <label>This action cannot be undone.</label>
+              </div>
+              <div class="modal-footer">
+                <div class="green-success-button">
+                  <button type="button" class="btn bg-primary btn-sm" data-dismiss="modal">Close</button>
+
+                </div>
+                <div class="green-success-button">
+                  <button class="btn bg-danger btn-sm" data-dismiss="modal" aria-label="Close" aria-hidden="true">Delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Modal -->
       </div>
     </div>
   </div>
@@ -111,15 +175,18 @@ export default {
   methods: {
     ...mapActions([
         'getSelectedLevel',
+        'getGame',
     ])
   },
   computed: {
     ...mapGetters([
         'getLevelInfo',
+        'getGameInfo',
     ])
   },
   created() {
     this.getSelectedLevel(this.$route.params.id);
+    this.getGame();
   }
 };
 </script>
@@ -130,7 +197,7 @@ export default {
 .game-category-card {
   margin: 15px;
   width: 100%;
-  min-height: 400px;
+  min-height: 500px;
   border-radius: 20px;
   background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
   box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
@@ -175,6 +242,27 @@ export default {
   outline: none;
   transition: 0.5s;
   box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+}
+
+.custom-select {
+  padding: 0 60px 0 60px;
+  border-radius: 0 12px 12px 0;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 100%;
+  border: none;
+  outline: none;
+  transition: 0.5s;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+}
+
+.create-update-date h5 {
+  text-align: center;
+  font-weight: bold;
+  font-size: 12px;
 }
 
 .green-success-button button {
