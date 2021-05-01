b1db410018707b28974fcfe37b424ee3ac8fc084
Emre Solmaz <emrslmz57@gmail.com>
2021-05-01 14:03:49 +0300

    added users request and auth simple

--- src/services/request/request_api.js
+++ src/services/request/request_api.js
@@ -1,6 +1,8 @@
 import base from '@/services/request/dashboard/base';
 
 export default {
+    users: `${base.base_url}/users`,
+
     shop_url: `${base.base_url}/items`,  //https://guess-what-rank-api.herokuapp.com/api/  +  items
     game_url: `${base.base_url}/games`,
     level_url: `${base.base_url}/levels`,
